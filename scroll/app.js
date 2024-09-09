6b83d5316640b85920e5c131d187c1a5904f5732
smierl <145398729+smierl@users.noreply.github.com>
2024-09-09 02:50:25 -0500

    Update app.js

--- scroll/app.js
+++ scroll/app.js
@@ -1,5 +1,8 @@
+// Agrega un evento de escucha para el evento 'scroll' en la ventana
 window.addEventListener('scroll', () => {
+    // Verifica si la ventana ha llegado a 100 píxeles antes del final del contenido del documento
     if (window.scrollY + window.innerHeight >= document.body.scrollHeight - 100) {
+        // Desplaza la ventana de vuelta al principio de la página
         window.scrollTo(0, 0);
     }
 });
